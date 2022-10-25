5e5b7175b837657b2e944eba786f6309e20211ef
Jederson Gabriel <89799559+Jeta8@users.noreply.github.com>
2022-10-25 10:37:32 -0300

    update 25/10

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.UI;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClassLibrary1
+{
+    internal class TagsDisponiveis
+    {
+        // TagsDisponiveis JanelaRevit = new TagsDisponiveis();
+        public void JanelaRevit(object sender, EventArgs e)
+        {
+            MessageBox.Show("Testando");
+            return;
+        }
+    }
+}
+
+
+public class ComandoTags : IExternalEventHandler
+{
+    public ExternalEvent cTags;
+
+    // Constructor
+    private ComandoTags()
+    {
+    }
+
+    public void Execute(UIApplication app)
+    {
+
+        // Aqui dentro é executado a lógica do programa
+        UserControl2 JanelaRevit = new UserControl2();
+        JanelaRevit.Show();
+
+    }
+
+    public string GetName()
+    {
+        return "Comando Tags";
+    }
+}
---
+++ ClassLibrary1/Class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+namespace SegundaBiblioteca
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            
+
+
+            return Result.Succeeded;
+        }
+    }
+}
